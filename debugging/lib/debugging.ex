2bf991cb18f9a872be307e8d6903111e65c60af2
Pablo Velez <pvelez@bp-3.com>
2019-07-21 10:15:05 -0500

    Used the debugger via :debugger.start() and added a break point using :int.ni(Debugging) and :int.break(Debugging, 3), then run it Debugging.double_sum(1,2)

--- debugging/lib/debugging.ex
+++ debugging/lib/debugging.ex
@@ -1,11 +1,13 @@
-require IEx;
 defmodule Debugging do
+  # Started the debugging in the iex -S mix using the :debugger.start() command and added a breakpoint via
+  # :int.ni(Debugging) && :int.break(Debugging, lineNumber)
   def double_sum(x, y) do
-    IEx.pry
     hard_work(x, y)
   end
 
   defp hard_work(x, y) do
-    2 * (x + y)
+    x = 2 * x
+    y = 2 * y
+    x + y
   end
 end
