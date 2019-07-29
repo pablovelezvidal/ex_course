3dcfe1e09445c1c751ed67ebe1afdd74cfe2278b
Pablo Velez <pvelez@bp-3.com>
2019-07-28 23:08:47 -0500

    Add scripts to work with agent and tasks and also created distributed nodes in the console by the use of Node.connect

---
+++ sect_9/tasks.exs
@@ -0,0 +1,14 @@
+defmodule Demo do
+  def work do
+    :timer.sleep(2000)
+    42
+  end
+end
+
+worker = Task.async(fn -> Demo.work end)
+IO.puts "we can still do something else"
+
+answer = Task.await worker
+
+IO.puts "The answer is #{answer}"
+
